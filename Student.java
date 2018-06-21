224dbbc802a41f04d819f4a7fac7e07598ab41f1
RobertSpiri <robert_spiri_v2@yahoo.com>
2018-06-21 20:28:04 +0300

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,53 @@
+package me.robert;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+
+import java.util.List;
+
+public class Student {
+    @Id
+    private String id;
+    private String name;
+    private String lastName;
+
+    public Student(){
+    }
+
+    public Student(String id, String name, String lastName) {
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+    @DBRef
+    private List<Curs> curs_ids;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String studentInfo() {
+        return "ID: " + id + "\nName: " + name + "\nLastName: " + lastName;
+    }
+}
